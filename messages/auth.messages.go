308234865d2df6a990e04b35354bdf24c39e8879
Nitin Goyal <nitin.goyal@ufl.edu>
2024-11-23 16:56:47 -0500

    remove deprecated message definition

--- messages/auth.messages.go
+++ messages/auth.messages.go
@@ -9,36 +9,3 @@ type Claims struct {
 	UserId 		uint 	`json:"user_id"`
 	Username 	string 	`json:"username"`
 }
-
-type RegisterRequest struct {
-	Username string
-	Email    string
-	Password string
-}
-
-type RegisterResponse struct {
-    ID   		uint
-    Username 	string
-    Error    	string
-}
-
-type LoginRequest struct {
-	Username string
-	Password string
-}
-
-type LoginResponse struct {
-	Token   string
-	Error   string
-}
-
-
-type TokenValidationRequest struct {
-	Token 	string
-}
-
-type TokenValidationResponse struct {
-	Valid  	bool
-	Claims 	*Claims
-	Error 	string
-}
